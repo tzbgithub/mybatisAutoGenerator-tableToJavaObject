6fd5e4eded339a1360f125e907dcd1c60e662717
564858834@qq.com <github19951108>
2020-11-28 21:45:38 +0800

    提交：数据表转化为java对象（mybatis自动生成mapper接口类、model类、mapper.xml）

---
+++ src/test/java/com/tzb/mall/ZhatangMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzb.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhatangMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
